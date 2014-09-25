0529051537189c170f84a007625b69d3b5fde1f7
sohel-github <tosohel22@gmail.com>
2014-09-25 19:38:30 +0600

    Added add another profile link in kids tab

--- profile.php
+++ profile.php
@@ -238,9 +238,46 @@
                   </div>
 
                 </div>
+
               </div>
             </div>
           </div>
+
+          <p><a href="" id="add_profile">Add Another Profile</a></p>
+
+          <div class="ui form" id = "add_another_profile" style="display:none;">
+
+            <div class="field">
+              <label class="cblabel">Full Name</label>
+              <div class="ui large fluid icon input">
+                  <input placeholder="Full Name" type="text">
+                  <i class="android icon"></i> 
+              </div>
+            </div>
+
+            <div class="field">
+              <label class="cblabel">Birth Date</label>
+              <div class="ui fluid icon input">
+                  <input placeholder = "Date of birth" type="text" id="dof">
+                  <i class="calendar icon"></i>
+              </div>
+            </div>
+
+            <div class="field">
+                <label class="cblabel">I am a</label>
+                <div class="ui fluid selection dropdown">
+                    <input name="gender" type="hidden">
+                        <div class="default text" style="text-align:left;">Gender</div>
+                        <i class="dropdown icon"></i>
+                        <div class="menu">
+                            <div class="item"><i class="male icon"></i> Male</div>
+                            <div class="item"><i class="female icon"></i> Female</div>
+                        </div>
+                </div>
+            </div>
+
+          </div>
+
         </div>
         <div class="two wide column"></div>
       </div>
@@ -253,6 +290,16 @@
 
 <script type="text/javascript">
   $( document ).ready(function() {
+
+    $('#add_profile').click(function(){
+      $('#add_another_profile').show();
+    });
+    
+    $( "#dof" ).datepicker({ 
+      changeMonth: true,
+      changeYear: true
+    });
+
     $('.demo.menu .item').tab();
     $('.ui.selection.dropdown').dropdown(); 
   });
